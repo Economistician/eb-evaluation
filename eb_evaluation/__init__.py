cff60a9d169760e0087079f446356474078aca9d
Kyle Corrie <kcorrie@economistician.com>
2025-12-06 15:01:34 -0500

    Create __init__.py
    Add eb_evaluation package initializer

---
+++ eb_evaluation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Electric Barometer Evaluation Toolkit (EB Evaluation)
+
+This package provides DataFrame-based evaluation utilities for model
+diagnostics, hierarchical evaluation, cost-based analysis, and
+entity-level forecasting performance.
+"""
